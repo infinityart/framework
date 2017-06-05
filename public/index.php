207e1c84956ec54d375760253a618d751eff8ff6
infinityart <jonty_wolf@hotmail.com>
2017-06-05 18:11:12 +0200

    Init of the framework.

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jonty
+ * Date: 6/5/2017
+ * Time: 6:01 PM
+ */
+
+declare(strict_types = 1);
+
+require __DIR__ . '/../src/bootstrap.php';
+
+echo 'Hello world';
---
+++ src/bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jonty
+ * Date: 6/5/2017
+ * Time: 6:05 PM
+ */
